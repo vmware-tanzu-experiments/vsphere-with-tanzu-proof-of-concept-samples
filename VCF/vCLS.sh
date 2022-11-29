c7fe3e9537d81802c25c735d5f124fc989832e3f
Dharmesh Bhatt <bhattd@bhattd-a01.vmware.com>
2022-11-29 14:22:31 +0000

    Update vCLS.sh

--- VCF/vCLS.sh
+++ VCF/vCLS.sh
@@ -6,43 +6,26 @@
 
 
 
-# check the input args
 
-if [[ $# -lt 2 ]]
-  then
-    echo "Usage: vCLS.sh <Cluster Name> <enable|disable> "
-    exit
-elif [[ $2 != 'disable' ]] && [[ $2 != 'enable' ]]
-  then
-    echo "Usage: vCLS.sh <Cluster Name> <enable|disable> "
-    exit
-fi
+usage() 
+{
+  echo "Usage: $0  -c 'cluster name' -s [enable|disable]" 1>&2
+  exit 1
+}
 
-
-
-# Check for govc
-# (verbose redirect for max. compat.)
-echo -n Checking for govc... 
-
-if govc version 2>/dev/null 1>/dev/null
-then 
-	echo $(govc version)
-else
-	echo No govc command found... you can find it here:
-	echo https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz
-	exit
-fi
-
-
-# check for vC access rights
-echo -n Checking for vC access...
-
-if 
-	govc about 2>/dev/null | grep 'Version' 
-then
-	echo
-else
+cluster_fail()
+{
 	echo Failed!
+	echo Please check the cluster name
+	echo Available clusters are:
+	govc find -type c | awk -F'/' '{print $NF}'
+	exit 1
+}
+
+
+vc_fail()
+{
+    echo Failed!
 	echo
 	echo You need to set govc env variables, e.g.:
 	echo
@@ -50,51 +33,81 @@ else
 	echo export GOVC_PASSWORD=your_vcenter_password
 	echo export GOVC_INSECURE=1
 	echo export GOVC_URL=your_vcenter_ip_address
-	exit
-fi
+	exit 1
+}
+
+govc_fail()
+{
+    echo No govc command found. 
+    echo You can download it here:
+	echo https://github.com/vmware/govmomi/releases/latest/download/govc_$(uname -s)_$(uname -m).tar.gz
+	exit 1
+}
+
+undefined_error()
+{
+	echo Hmmm...something went wrong, sorry
+	exit 1
+}
+
+
+# check the input args
+
+! [[ ${#} -eq 4 ]] && usage
+
+while getopts ":c:s:" options; do
+  case "${options}" in
+    c)
+      cluster=${OPTARG}
+      ;;
+    s)
+      state=${OPTARG}
+      if [[ $state != 'disable' ]] && [[ $state != 'enable' ]]
+      then
+        usage
+      fi
+      ;;
+    :)
+      echo "Error: -${OPTARG} requires an argument."
+      usage
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
 
 
 
+# Check for govc
+# (verbose redirect for max. compat.)
+echo -n Checking for govc... 
+[[ $(govc version) ]] && echo OK || govc_fail
 
-cluster=$1
+# check for vC access rights
+echo -n Checking for vC access...
+[[ $(govc about 2>/dev/null | grep 'Version') ]] && echo OK || vc_fail
 
 # make sure the cluster exists
+echo -n Checking for Cluster...
+[[ $(govc find -type c | grep -oE "\<$cluster\$") ]] && echo OK || cluster_fail
 
-echo -n Checking for cluster...
-
-if govc find -type c | grep $cluster 2>/dev/null
-then
-	echo \>\>found cluster $cluster
-else
-	echo Failed!
-	echo Please check the cluster name
-	echo Available clusters are:
-	govc find -type c | awk -F'/' '{print $NF}'
-	exit
-fi
-
-
-echo Setting vCLS state in $cluster to $2
-domain=$(govc find -verbose=true -type c 2> >(grep $cluster) | grep -oEm 1 'domain-c\w+')
-
-echo \>\>Cluster $cluster translates to MOB object $domain
+# find the domain
+echo Setting vCLS state in $cluster to $state
+domain=$(govc find -verbose=true -type c 2> >(grep -E "\<$cluster\$") | grep -oEm 1 'domain-c\w+')
+[[ $(echo $?) -eq 0 ]] && echo \>\>Cluster $cluster translates to MOB object $domain || undefined_error
 	
 # Do the thing 
-
-if [[ "$2" == "disable" ]]
+if [[ "$state" == "disable" ]]
 then
 	govc option.set config.vcls.clusters.$domain.enabled false
-elif [[ "$2" == "enable" ]]
+elif [[ "$state" == "enable" ]]
 then
 	govc option.set config.vcls.clusters.$domain.enabled true
 else
-	echo Hmmm...something went wrong, sorry
-	exit
+    undefined_error
 fi
 
-if [[ $(echo $?) -eq 0 ]]
-then echo Done!
-else echo Sorry, something went wrong
-fi
+[[ $(echo $?) -eq 0 ]] && echo Done! || undefined_error
 
 
